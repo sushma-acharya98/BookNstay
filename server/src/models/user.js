a8407e55efc4ab5bb75f4f34b925a092fd8c1271
Sushma Acharya <sushmaacharya345@gmail.com>
2024-01-17 07:14:28 +0545

    Update user model for hotel booking app

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  phoneNumber: {type:String, unique: true, required: true},
-  email: String,
+  email: { type: String, unique: true, required: true },
   fullName: String,
-  role: {
-    type: String,
-    enum : ['user', 'admin', 'rider'],
-    default : 'user'
-  },
   password: String
 });
 
 const User = mongoose.model('User', userSchema);
-module.exports = User
+
+module.exports = User;
