72ec1b6c6aaeedb2b05beb4a1c7b098df7c81a80
Sushma Acharya <sushmaacharya345@gmail.com>
2024-02-06 15:53:31 +0545

    made some changes in dashboard

--- client/src/app/dashboard/roomManagement/page.js
+++ client/src/app/dashboard/roomManagement/page.js
@@ -1,21 +1,21 @@
-// components/RoomManagement.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// // components/RoomManagement.js
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-const RoomManagement = () => {
-  const [rooms, setRooms] = useState([]);
+// const RoomManagement = () => {
+//   const [rooms, setRooms] = useState([]);
 
-  useEffect(() => {
+//   useEffect(() => {
    
-    axios.get('/api/rooms').then((response) => setRooms(response.data));
-  }, []);
+//     axios.get('/api/rooms').then((response) => setRooms(response.data));
+//   }, []);
 
-  return (
-    <div>
-      <h3>Room Management</h3>
-      {/*  how to Display rooms here  */}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h3>Room Management</h3>
+//       {/*  how to Display rooms here  */}
+//     </div>
+//   );
+// };
 
-export default RoomManagement;
+// export default RoomManagement;
